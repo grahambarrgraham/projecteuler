d3550184ac73bdc8d34be88a2f187debab87b738
gbarr <graham.barr@skyscanner.net>
2024-11-16 15:54:57 +0000

    86

--- problems/problem_75_singular_integer_right_trianges.py
+++ problems/problem_75_singular_integer_right_trianges.py
@@ -4,16 +4,17 @@ from collections import defaultdict
 from time import time
 
 
-def pythagorean_primitive_triplets_gen(hypotenuse_limit):
+# a, b and c are co-prime, and a <= b <= c
+def pythagorean_primitive_triplets_gen(limit_func):
     a, b, c, m = 0, 0, 0, 2
 
-    while c <= hypotenuse_limit:
+    while limit_func(a, b, c):
         for n in range(1, m):
             a = m * m - n * n
             b = 2 * m * n
             c = m * m + n * n
 
-            if c > hypotenuse_limit:
+            if not limit_func(a, b, c):
                 break
 
             if math.gcd(a, b, c) == 1:
@@ -22,25 +23,34 @@ def pythagorean_primitive_triplets_gen(hypotenuse_limit):
         m = m + 1
 
 
-def pythagorean_triplets(circumference_limit: int) -> set[tuple]:
+# Currently need separate functions to limit generated primitives and non-primitives. as the primitive function
+# needs to be more permissive.  Leaky abstraction.
+def pythagorean_triplets(primitives_limit_func, k_limit_func) -> set[tuple]:
 
-    def wire_limit(a_, b_, c_):
-        return a_ + b_ + c_ <= circumference_limit
+    primitives = set(pythagorean_primitive_triplets_gen(primitives_limit_func))
 
-    primitives = set(pythagorean_primitive_triplets_gen(circumference_limit))
+    # to derive all pythagorean triples from primitives, we can multiply a, b and c by k
 
     result_ = set()
     for a, b, c in primitives:
         k = 1
-        while wire_limit(ka := k * a, kb := k * b, kc := k * c):
+        while k_limit_func(ka := k * a, kb := k * b, kc := k * c):
             result_.add((ka, kb, kc))
             k += 1
 
+    # a <= b <= c
     return result_
 
 
+def pythagorean_triplets_(circumference_limit: int) -> set[tuple]:
+    def wire_limit(a_, b_, c_):
+        return a_ + b_ + c_ <= circumference_limit
+
+    return pythagorean_triplets(lambda a, b, c: c <= circumference_limit, wire_limit)
+
+
 def singular_integer_right_triangles(limit):
-    triplets = pythagorean_triplets(limit)
+    triplets = pythagorean_triplets_(limit)
     circumference_to_solutions = defaultdict(int)
     for a, b, c in triplets:
         circumference_to_solutions[a + b + c] += 1
--- problems/problem_86_cuboid_route.py
+++ problems/problem_86_cuboid_route.py
@@ -1,94 +1,51 @@
 # https://projecteuler.net/problem=86
-import math
-from itertools import permutations
 from time import time
 
-from problems.problem_75_singular_integer_right_trianges import pythagorean_primitive_triplets_gen
+from problems.problem_75_singular_integer_right_trianges import pythagorean_triplets
 
 
-def build_cuboids(m: int):
-    seen = set()
-    cuboids = []
-    for x in range(1, m + 1):
-        for y in range(1, m + 1):
-            for z in range(1, m + 1):
-                t = (x, y, z)
-                if t not in seen:
-                    rotations = set(permutations(t, 3))
-                    seen.update(rotations)
-                    cuboids.append(t)
-    return cuboids
+# produces all triples such that a <= b <= c, a <= side_length, b <= side_length
+def pythagorean_triples_(side_limit: int) -> set[tuple]:
+    return pythagorean_triplets(
+        lambda a, b, c: a <= side_limit,
+        lambda a, b, c: a <= side_limit // 2 and b <= side_limit)
 
 
-def shortest_path(x: int, y: int, z: int):
-    solution_1 = (x + z) ** 2 + y ** 2
-    solution_2 = x ** 2 + (y + z) ** 2
-    solution_3 = (x + y) ** 2 + z ** 2
-    return (x, y, z), math.sqrt(min(solution_1, solution_2, solution_3))
-
-
-def cuboid_route(cuboids):
-    cuboids_short_path = [shortest_path(*cuboid) for cuboid in cuboids]
-    return [(cuboid, path_length) for cuboid, path_length in cuboids_short_path if path_length.is_integer()]
-
-
-def pythagorean_pairs(limit: int) -> set[tuple]:
-    def side_limit(a_, b_):
-        return (a_ <= limit and b_ <= limit * 2) or (b_ <= limit and a_ <= limit * 2)
-
-    primitives = set(pythagorean_primitive_triplets_gen(limit * 100))
-
-    result_ = set()
-    for a, b, c in primitives:
-        k = 1
-        while side_limit(ka := k * a, kb := k * b):
-            result_.add((ka, kb))
-            k += 1
-
-    return result_
-
-
-def find_cubes(pair, m, integer_side_pairs):
-    a, b = pair
-    cubes_ = set()
-    if b <= m:
-        for i in range(1, a):
-            x, y, z = sorted([i, a - i, b])
-            if x <= m and y <= m and z <= m:
-                cubes_.add((x, y, z))
-    if a <= m:
-        for i in range(1, b):
-            x, y, z = sorted([i, b - i, a])
-            if x <= m and y <= m and z <= m:
-                cubes_.add((x, y, z))
-
-    return cubes_
-
-
-def build_cuboids_v2(m):
-    pairs = pythagorean_pairs(m)
-    # print(sorted(pairs))
+def find_cubes(triple, m) -> set[tuple]:
+    a, b, _ = triple
     result = set()
-    for triplet in pairs:
-        cubes = find_cubes(triplet, m, pairs)
-        result.update(cubes)
+
+    # the shortest path is √(x² + y²) + √z² if x <= y <= z
+
+    def generate_cubes(p, q):
+        for i in range(1, m):
+            x, y, z = [i, p - i, q]
+            if x <= y <= z:
+                result.add(tuple((x, y, z)))
+
+    if b == m:
+        generate_cubes(a, b)
+    if a == m:
+        generate_cubes(b, a)
+
     return result
 
 
+def build_cuboids(m, triples) -> set[tuple]:
+    return {cuboid for triple in triples for cuboid in find_cubes(triple, m)}
+
+
+def cuboid_route(target_cuboids, limit_m=1900) -> int:
+    # generate more triples than required, up to a limit correlated to m, the cube edge length
+    # multiply limit_m by two as the flattened cube requires 2 sides of the cube to form one edge of the triple
+    triples = pythagorean_triples_(limit_m * 2)
+    num_cuboids, m = 0, 0
+    while num_cuboids < target_cuboids:
+        m += 1
+        cuboids = build_cuboids(m, triples)
+        num_cuboids += len(cuboids)
+    return m
+
+
 now = time()
-for lim in range(100, 101):
-    v1 = cuboid_route(build_cuboids(lim))
-    v2_cuboids = build_cuboids_v2(lim)
-    v2 = cuboid_route(v2_cuboids)
-    v3 = v2_cuboids
-
-    print(len(v1))
-    print(len(v2))
-    print(len(v3))
-
-# print(sorted(v1))
-# print(sorted(v2))
-# print(sorted(v1.difference(v2)))
-
-    print(lim, len(v2), f"{round(time() - now, 2)} seconds")
-# print(math.sqrt((105 ** 2) + (100 ** 2)))
+print(cuboid_route(1000000), f"{round(time() - now, 2)} seconds")
